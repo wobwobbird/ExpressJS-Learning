a7973d5f59e433de483ab2a2778822d4481ac6c8
wobwobbird <36921161+wobwobbird@users.noreply.github.com>
2025-11-25 18:42:14 +0000

    setting up app

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middlewear
+app.use(express.json());
+
+// basic route
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello Express' });
+});
+
+//start server
+app.listen(PORT, () => {
+    console.log(`Server is runninggggg on http://localhost:${PORT}`);
+});
